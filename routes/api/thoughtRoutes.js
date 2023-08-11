43569c666dce4789a06c4f1fadde49a581d3c8c1
Elias Rivera <eliasjohnrivera@gmail.com>
2023-08-10 17:33:17 -0700

    added routes/api

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { 
+    getAllThoughts, 
+    getThoughtById, 
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction);
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+
+module.exports = router;
